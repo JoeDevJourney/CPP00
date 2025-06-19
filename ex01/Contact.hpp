4ba8db46f496970fa39ca752102bc0cc6f15d1af
Johannes Brandt <jbrandt@2-F-7.42heilbronn.de>
2025-06-19 17:20:11 +0200

    did some more work on the hpp file for contact

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -1,16 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   contact.hpp                                        :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jbrandt <jbrandt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 13:23:10 by jbrandt           #+#    #+#             */
-/*   Updated: 2025/06/19 13:40:40 by jbrandt          ###   ########.fr       */
+/*   Updated: 2025/06/19 17:17:34 by jbrandt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
+
+#include <string>
+#include <iostream>
+
+class Contact {
+private:
+	std::string FirstName;
+	std::string LastName;
+	std::string Nickname;
+	std::string PhoneNumber;
+	std::string DarkestSecret;
+
+public:
+	Contact();
+
+};
 
 #endif
