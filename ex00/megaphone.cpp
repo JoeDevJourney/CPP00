30eadcca9f389060acf9b2c57c2834f0df667c38
Johannes Brandt <jbrandt@2-F-7.42heilbronn.de>
2025-06-26 18:03:38 +0200

    changed my ex00 and made it more cpp like

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,12 +6,19 @@
 /*   By: jbrandt <jbrandt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 11:45:21 by jbrandt           #+#    #+#             */
-/*   Updated: 2025/06/19 12:55:59 by jbrandt          ###   ########.fr       */
+/*   Updated: 2025/06/26 17:59:37 by jbrandt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string.h>
+#include <cstring>
+#include <string>
+
+void	print_megaphone(const std::string str)
+{
+	for (std::string::const_iterator it = str.cbegin(); it < str.cend(); ++it)
+		std::cout << char(std::toupper(*it));
+}
 
 int	main(int argc, char *argv[])
 {
@@ -20,13 +27,10 @@ int	main(int argc, char *argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
+	
 	for (int i = 1; i < argc; i++)
-	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
-		{
-			std::cout << char(std::toupper(argv[i][j]));
-		}
-	}
+		print_megaphone(argv[i]);
+
 	std::cout << std::endl;
 	return (0);
 }
