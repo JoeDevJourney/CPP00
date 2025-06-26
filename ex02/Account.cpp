8730823bcf80b097f7d7d6f8d3d39809ad19c4e2
Johannes Brandt <jbrandt@2-F-7.42heilbronn.de>
2025-06-26 19:35:44 +0200

    added display_timestamp function and currently working on displayAccountinfos

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,13 +6,14 @@
 /*   By: jbrandt <jbrandt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 13:25:17 by jbrandt           #+#    #+#             */
-/*   Updated: 2025/06/26 14:54:44 by jbrandt          ###   ########.fr       */
+/*   Updated: 2025/06/26 19:34:12 by jbrandt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
 #include <ctime>
+#include <iomanip>
 
 int Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
@@ -27,4 +28,28 @@ Account::Account(int intial_deposit)
 {
 	_nbAccounts++;
 	_totalAmount += intial_deposit;
+}
+
+void Account::_displayTimestamp()
+{
+	std::time_t now = std::time(nullptr);
+	std::tm*	ltm = std::localtime(&now);
+	std::cout << '['
+		<< 1900 + ltm->tm_year
+		<< std::setw(2) << std::setfill('0') << ltm->tm_mon + 1
+		<< std::setw(2) << std::setfill('0') << ltm->tm_mday << '_'
+		<< std::setw(2) << std::setfill('0') << ltm->tm_hour
+		<< std::setw(2) << std::setfill('0') << ltm->tm_min
+		<< std::setw(2) << std::setfill('0') << ltm->tm_sec
+		<< "]";
+}
+
+void Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts
+		<< ";total:" << _totalAmount
+		<< ";deposits:" << _totalNbDeposits
+		<< ";withdrawals:" << _totalNbWithdrawals
+		<< std::endl;
 }
